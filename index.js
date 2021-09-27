66e462f29898550c324b39ff5bb67b8a48476d0e
James <jamesschmidt124@gmail.com>
2021-09-27 14:31:36 -0700

    commit tree 249b32d9a99f7023ce05d54cad1b7a27cd399a98

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Write your code here!
+main.remove()
+// const newHeader = document.createElement('h1');
+// newHeader.id = 'victory';
+// newHeader.innerHTML = "YOUR-NAME is the champion";
 const newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.innerHTML = "YOUR-NAME is the champion";
+newHeader.innerHTML = 'YOUR-NAME is the champion';
